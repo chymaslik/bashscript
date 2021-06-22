51db31bdbcf4f920d9eaa182ca79aafeee23f3cb
Aleksey Chumaslov <chymaslik@gmail.com>
2021-06-22 14:01:17 +0300

    modified myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+web1=mynginx_$(date +%Y%m%d%H%M%S):1.0.1
 docker build -t mynginx_$(date +%Y%m%d%H%M%S):1.0.1 -f Dockerfile.web1 .
+docker run -d -p 1111:80 $web1 
+web2=mynginx_$(date +%Y%m%d%H%M%S):2.0.2
 docker build -t mynginx_$(date +%Y%m%d%H%M%S):2.0.2 -f Dockerfile.web2 .
+docker run -d -p 2222:80 $web2
