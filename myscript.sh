84c6f9e7dd7617ae8177c3d2eeaaff8addc617cd
Aleksey Chumaslov <chymaslik@gmail.com>
2021-06-22 13:33:29 +0300

    modify myscript

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t mynginx_$(date +%Y%m%d%H%M%S):1.0.1 -f Dockerfile.web1
-docker build -t mynginx_$(date +%Y%m%d%H%M%S):1.0.1 -f Dockerfile.web2
+docker build -t mynginx_$(date +%Y%m%d%H%M%S):1.0.1 -f Dockerfile.web1 .
+docker build -t mynginx_$(date +%Y%m%d%H%M%S):2.0.2 -f Dockerfile.web2 .
