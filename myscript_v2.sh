55a14e9807158179bfb1573388c4c8785acdeefb
Aleksey Chumaslov <chymaslik@gmail.com>
2021-06-24 16:39:05 +0300

    add x

--- myscript_v2.sh
+++ myscript_v2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+x=0
 for file in ./Dockerfile.*
 do
-docker build -t mynginx_$(date +%Y%m%d%H%M%S):1.0.1 -f $file .
-docker run -d -P mynginx_$(date +%Y%m%d%H%M%S):1.0.1
+docker build -t mynginx_$(date +%Y%m%d%H%M%S):1.0.$x -f $file .
+docker run -d -P mynginx_$(date +%Y%m%d%H%M%S):1.0.$x
+x=x+1
 sleep 5
 done
