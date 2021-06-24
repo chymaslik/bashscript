0f1566e7130ff79f2fc3081d8817703c91b876b2
Aleksey Chumaslov <chymaslik@gmail.com>
2021-06-24 17:02:57 +0300

    modify port

--- myscript_v2.sh
+++ myscript_v2.sh
@@ -5,7 +5,7 @@ for file in ./Dockerfile.*
 do
 t=$(date +%Y%m%d%H%M%S)
 docker build -t mynginx_$t:1.0.$x -f $file .
-docker run -d -p $port mynginx_$t:1.0.$x
+docker run -d -p $port:80 mynginx_$t:1.0.$x
 ((x++))
 ((port=port+1111))
 sleep 1
