7806379055d44f9a14c18788a2ae5fdfb2dac871
Aleksey Chumaslov <chymaslik@gmail.com>
2021-06-24 17:27:09 +0300

    add check

--- myscript_v2.sh
+++ myscript_v2.sh
@@ -3,6 +3,10 @@ x=0
 port=1111
 for file in ./Dockerfile.*
 do
+    if lsof | grep $port
+    then
+    ((port=port+1111))
+    fi
 t=$(date +%Y%m%d%H%M%S)
 docker build -t mynginx_$t:$x.0.$x -f $file .
 docker run -d -p $port:80 mynginx_$t:$x.0.$x
