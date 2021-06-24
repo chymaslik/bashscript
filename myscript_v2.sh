243482c30e53c9c4f208eb6640949cb471176ec1
Aleksey Chumaslov <chymaslik@gmail.com>
2021-06-24 17:41:38 +0300

    modify myscript_v2

--- myscript_v2.sh
+++ myscript_v2.sh
@@ -3,10 +3,6 @@ x=0
 port=1111
 for file in ./Dockerfile.*
 do
-    if grep $port lsof
-    then
-    ((port=port+1111))
-    fi
 t=$(date +%Y%m%d%H%M%S)
 docker build -t mynginx_$t:$x.0.$x -f $file .
 docker run -d -p $port:80 mynginx_$t:$x.0.$x
