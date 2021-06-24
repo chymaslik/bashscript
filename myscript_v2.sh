8409696cc84faf20759bb6d77e78913ede80f8dc
Aleksey Chumaslov <chymaslik@gmail.com>
2021-06-24 17:01:04 +0300

    add port

--- myscript_v2.sh
+++ myscript_v2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 x=0
+port=1111
 for file in ./Dockerfile.*
 do
 t=$(date +%Y%m%d%H%M%S)
 docker build -t mynginx_$t:1.0.$x -f $file .
-docker run -d -P mynginx_$t:1.0.$x
+docker run -d -p $port mynginx_$t:1.0.$x
 ((x++))
+((port=port+1111))
 sleep 1
 done
